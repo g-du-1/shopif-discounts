32ca922550d637dcbd35a3a2db9c95414f7f1cc0
g-du-1 <dudaszeg1@gmail.com>
2022-05-23 17:05:36 +0100

    Add theme app extension code

---
+++ theme-app-extension/assets/shopify-discounts.js
@@ -0,0 +1,29 @@
+(async () => {
+  console.log("Theme app extension loading...");
+
+  const fetchCart = async () => {
+    const response = await fetch("/cart.js");
+    if (!response.ok) {
+      const message = `An error has occured: ${response.status}`;
+      throw new Error(message);
+    }
+    const data = await response.json();
+    return data;
+  };
+
+  const fetchOffers = async () => {
+    const response = await fetch("/apps/shopif-discounts/api/v1/offers");
+    if (!response.ok) {
+      const message = `An error has occured: ${response.status}`;
+      throw new Error(message);
+    }
+    const data = await response.json();
+    return data;
+  };
+
+  const cart = await fetchCart();
+  console.log(cart);
+
+  const offers = await fetchOffers();
+  console.log(offers);
+})();
